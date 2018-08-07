c55b844e528d59497dc416bab2598410cf01a6e7
seamus-quinn <sumaesquinn@gmail.com>
2018-08-07 12:37:44 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,8 +1,17 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day
+	"Happy Mother's day, Mom! I hope you have a great day!"
+end
+
+## Part Two - Mother's Day with One Argument
+def mothers_day(name)
+	"Happy mothers day, #{name}! I hope you have a great day!"
+end
+
 ## Part Three - Mother's Day with One Optional Argument
 def mothers_day(name="Mom")
-	puts "Happy Mother's Day, #{name}!"
+	puts "Happy mothers day, #{name}! I hope you have a great day!"
 end
 
 mothers_day
